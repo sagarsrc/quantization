c70d83de80f79405926b671289fb9684ca280770
Sagar Sarkale <sagarsarkale.work@gmail.com>
2025-04-02 05:30:47 +0000

    [add] quantization initial commit

--- setup.sh
+++ setup.sh
@@ -1,12 +1,21 @@
 # Install CUDA and NVIDIA utilities
+apt-get install -y build-essential cmake
+
+
 wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb -O /tmp/cuda-keyring_1.1-1_all.deb && \
 dpkg -i /tmp/cuda-keyring_1.1-1_all.deb && \
 apt-get update && \
 apt-get install -y --no-install-recommends \
     cuda-nvcc-12-4 \
     cuda-cudart-dev-12-4 \
-    nvidia-utils-535
+    nvidia-utils-535 \
+    cuda-libraries-dev-12-4 \
+    cuda-compiler-12-4 \
+    libcudnn8 \
+    libcudnn8-dev
 rm -rf /tmp/cuda-keyring_1.1-1_all.deb
+
+export PATH=/usr/local/cuda-12.4/bin:$PATH
 
 # Install UV
 echo "Installing UV..."
@@ -27,9 +36,7 @@ uv --version || { echo "UV installation is broken." >&2; exit 1; }
 # Create and activate virtual environment
 echo "Setting up virtual environment..."
 uv venv .venv --python=3.10
-check_status "Virtual environment creation"
 source .venv/bin/activate
-check_status "Virtual environment activation"
 
 # Install bitsandbytes
 echo "Installing bitsandbytes..."
